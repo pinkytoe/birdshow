568df020e1c89d77f5e39fef93591cca7ec48bf5
Bertram Beyer <bertram.beyer@sap.com>
2016-04-18 19:01:09 +0200

    Show images

--- com.github.pinkytoe.birdshow/src/com/github/pinkytoe/birdshow/web/WebServer.java
+++ com.github.pinkytoe.birdshow/src/com/github/pinkytoe/birdshow/web/WebServer.java
@@ -1,9 +1,10 @@
 package com.github.pinkytoe.birdshow.web;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-
-import javax.imageio.ImageIO;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -11,38 +12,48 @@ import com.sun.net.httpserver.HttpServer;
 
 public class WebServer {
 
-	public void start() throws IOException{
+	public void start() throws IOException {
 		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 		server.setExecutor(null); // creates a default executor
 		server.createContext("/", new RootHandler());
+		server.createContext("/img", new ImageHandler());
 		server.start();
-		
+
 	}
-	
-	static class RootHandler implements HttpHandler{
+
+	static class RootHandler implements HttpHandler {
 
 		@Override
 		public void handle(HttpExchange ex) throws IOException {
-			String response = "some response";
+			String response = "some <b>response</b> <img src=\"/img\" />";
+			ex.getResponseHeaders().set("Content-Type", "text/html;charset=utf-8");
 			ex.sendResponseHeaders(200, response.length());
 			OutputStream os = ex.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-			
-		}
-		
-	}
-	
-	static class ImageHandler implements HttpHandler{
+			os.write(response.getBytes());
+			os.close();
 
-		@Override
-		public void handle(HttpExchange arg0) throws IOException {
-			// TODO Auto-generated method stub
-//			ex.sendResponseHeaders(200, response.length());
-//			OutputStream os = ex.getResponseBody();
-//			ImageIO.write(im, formatName, output);
-//			os.close();
 		}
-		
+
+	}
+
+	static class ImageHandler implements HttpHandler {
+
+		//http://stackoverflow.com/questions/16228109/how-can-you-stream-m-jpeg-from-a-java-httpserver
+		@Override
+		public void handle(HttpExchange ex) throws IOException {
+
+			File file = new File("C:\\Users\\D058643\\Pictures\\Cookie_monster.jpg").getCanonicalFile();
+			ex.sendResponseHeaders(200, 0);
+			OutputStream os = ex.getResponseBody();
+			FileInputStream fs = new FileInputStream(file);
+			final byte[] buffer = new byte[0x10000];
+			int count = 0;
+			while ((count = fs.read(buffer)) >= 0) {
+				os.write(buffer, 0, count);
+			}
+			fs.close();
+			os.close();
+
+		}
 	}
 }
